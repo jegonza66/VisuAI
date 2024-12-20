00f9901308cab5c556e4a13f4f51e1ebb8752172
Joac <joaquin.gonzalez6693@gmail.com>
2024-12-19 21:06:03 -0300

    Implemented yolo. Implemented functions in module. Implemented hierarchical multiple models

--- functions.py
+++ functions.py
@@ -2,6 +2,211 @@ import numpy as np
 import cv2
 import tensorflow as tf
 from tensorflow.keras.applications.inception_v3 import preprocess_input
+import torch
+from util.util import tensor2im
+from tensorflow.keras.applications import InceptionV3
+from ultralytics import YOLO
+import sys
+from options.test_options import TestOptions
+import tensorflow_hub as hub
+from torchvision import transforms
+from models import create_model
+# import mediapipe as mp
+
+
+def define_models_params(model_name, img_load_size, output_width, output_height, save_output_path, dream_model_layer):
+    models = {}
+    params = {}
+    # YOLO model
+    if 'yolo' in model_name:
+        # Load the YOLO11 model
+        yolo_model = YOLO("yolo11n.pt")
+        models['yolo_model'] = yolo_model
+
+    # Style transfer model
+    if 'style_transfer' in model_name:
+        # Load the pre-trained style transfer model
+        style_transfer_model = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
+        style_image_path = 'input/style.jpg'
+        prev_style_image = cv2.imread(style_image_path)  # Replace with your style image path
+        prev_style_image = cv2.cvtColor(prev_style_image, cv2.COLOR_BGR2RGB)
+
+        models['style_transfer_model'] = style_transfer_model
+        params['style_image_path'] = style_image_path
+        params['prev_style_image'] = prev_style_image
+
+    # Cyclegans models
+    if 'cyclegan' in model_name:
+        # Define model parameters
+        sys.argv = [
+            'test.py',  # Script name
+            '--no_dropout',
+            '--gpu_ids', '0',
+        ]
+
+        # Define gpu or cpu device
+        opt = TestOptions().parse()  # Get default options
+        opt.name = model_name.split('cyclegan_')[-1]
+        opt.load_size = img_load_size
+        opt.output_width = output_width
+        opt.output_height = output_height
+        opt.save_output_path = save_output_path
+
+        # Define transforms for webcam frames
+        transform = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.Resize((img_load_size, img_load_size)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+
+        # Initialize CycleGAN model
+        cyclegan_model = create_model(opt)  # Create the CycleGAN model
+        cyclegan_model.setup(opt)  # Load the pre-trained weights
+
+        models['cyclegan_model'] = cyclegan_model
+        params['opt'] = opt
+        params['transform'] = transform
+
+    # DeepDream model
+    if 'dream' in model_name:
+        # Load pre-trained InceptionV3 model
+        model = InceptionV3(include_top=False, weights='imagenet')
+        dream_layer = model.get_layer(f'mixed{dream_model_layer}')  # Layer to "dream" from
+        dream_model = tf.keras.Model(inputs=model.input, outputs=dream_layer.output)
+
+        models['dream_model'] = dream_model
+        params['deam_layer'] = dream_layer
+
+    # # Cartoon model
+    # if 'cartoon' in model_name:
+    #     # Initialize Mediapipe Face Detection
+    #     mp_face_detection = mp.solutions.face_detection
+    #     face_detection = mp_face_detection.FaceDetection(min_detection_confidence=0.5)
+    #
+    #     params['face_detection'] = face_detection
+
+    return models, params
+
+def transform_frame_style_transfer(models, frame, img_load_size, style_image_path, prev_style_image, output_width, output_height):
+
+    # Unpack models
+    style_transfer_model = models['style_transfer_model']
+
+    # Resize frame
+    frame = cv2.resize(frame, (img_load_size, img_load_size))
+
+    # Load new style image
+    style_image = cv2.imread(style_image_path)  # Replace with your style image path
+    # If loading failed, stick to previous style
+    if not isinstance(style_image, np.ndarray):
+        style_image = prev_style_image
+    # If it worked, update previous style
+    elif style_image.shape != prev_style_image.shape:
+        prev_style_image = style_image
+    elif (style_image != prev_style_image).any():
+        prev_style_image = style_image
+
+    style_image = cv2.cvtColor(style_image, cv2.COLOR_BGR2RGB)
+
+    # Convert frame to RGB
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    # Apply style transfer
+    frame = apply_style_transfer(frame, style_image, style_transfer_model, image_size=img_load_size)
+
+    # Convert back to BGR for OpenCV display
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    # Resize output frame
+    frame = cv2.resize(frame, (output_width, output_height))
+
+    return frame
+
+def transform_frame_cyclegan(models, frame, img_load_size, opt, transform, output_width, output_height):
+    # Unpack model
+    cyclegan_model = models['cyclegan_model']
+
+    # Resize frame
+    frame = cv2.resize(frame, (img_load_size, img_load_size))
+
+    # Convert to rgb
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    # Transform with model here
+    input_tensor = transform(frame).unsqueeze(0).to(torch.device("cuda" if opt.gpu_ids else "cpu"))
+
+    # Generate the transformed image
+    cyclegan_model.set_input({'A': input_tensor})  # Set the input image
+    cyclegan_model.test()  # Perform inference
+    output_image = tensor2im(cyclegan_model.get_current_visuals()['fake'])
+
+    # Convert the output image to a format suitable for OpenCV
+    frame = cv2.cvtColor(output_image, cv2.COLOR_RGB2BGR)
+    # Resize output frame
+    frame = cv2.resize(frame, (output_width, output_height))
+
+    return frame
+
+def transform_frame_yolo(models, frame):
+
+    # unpack model
+    yolo_model = models['yolo_model']
+
+    # Run YOLO11 tracking on the frame, persisting tracks between frames
+    results = yolo_model.track(frame, persist=True, verbose=False)
+
+    # Visualize the results on the frame
+    frame = results[0].plot()
+
+    return frame
+
+def transform_frame_pych(frame, frame_count, amplitude=20, wavelength=75, frame_count_div=5):
+    # Apply hue shift
+    hue_shifted = hue_shift(frame, (frame_count) % 180)
+
+    # Add ripple effect
+    rippled = animated_ripple_effect(hue_shifted, frame_count=frame_count, amplitude=amplitude, wavelength=wavelength)
+
+    # Apply gradient map
+    psychedelic = animated_gradient_map(rippled, frame_count / frame_count_div)
+
+    return psychedelic
+
+def transform_frame_dream(models, frame, img_load_size, output_width, output_height):
+
+    # Unpack model
+    dream_model = models['dream_model']
+    # Resize frame
+    frame = cv2.resize(frame, (img_load_size, img_load_size))
+    # Convert frame to RGB
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    # Apply DeepDream effect (downscale for efficiency)
+    frame = apply_deepdream(frame, dream_model, img_size=img_load_size)
+    # Convert back to BGR for OpenCV display
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    # Resize output frame
+    frame = cv2.resize(frame, (output_width, output_height))
+
+    return frame
+
+def transform_frame_cartoon(frame, face_effects, face_detection, face_text):
+
+    frame = cartoonify(frame)
+
+    # Detect faces
+    if face_effects:
+        results = face_detection.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        face_coords = []
+        if results.detections:
+            for detection in results.detections:
+                bbox = detection.location_data.relative_bounding_box
+                face_coords.append((bbox.xmin, bbox.ymin, bbox.xmin + bbox.width, bbox.ymin + bbox.height))
+
+        # Add math effects
+        if face_text:
+            frame = add_math_effect(frame, face_coords, text=face_text)
+
+    return frame
 
 
 def brighten_dark_regions(image, threshold=50, factor=1.5):
@@ -33,6 +238,7 @@ def deepdream(image, dream_model, iterations=2, step_size=.2):
         grads /= tf.math.reduce_std(grads) + 1e-8
         image = image + grads * step_size
     return image
+
 # Prepare image for model
 def preprocess_image(image, size):
     # Preprocess: Adjust brightness/contrast
@@ -55,7 +261,7 @@ def apply_deepdream(frame, dream_model, img_size):
     return np.array(dreamed_image)
 
 
-# Style transfer functions
+#----- Style transfer functions -----#
 def apply_style_transfer(content_image, style_image, style_transfer_model, image_size):
     # Preprocess images
     content_image = tf.image.resize(content_image, (image_size, image_size)) / 255.0
@@ -109,20 +315,6 @@ def hue_shift(image, shift_value):
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     hsv[:, :, 0] = (hsv[:, :, 0] + shift_value) % 180  # Shift hue channel
     return cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-
-
-def psychedelic_effect(image, frame_count, amplitude=3, wavelength=15, frame_count_div=4):
-    # Apply hue shift
-    hue_shifted = hue_shift(image, (frame_count) % 180)
-
-    # Add ripple effect
-    rippled = animated_ripple_effect(hue_shifted, frame_count=frame_count, amplitude=amplitude, wavelength=wavelength)
-
-    # Apply gradient map
-    psychedelic = animated_gradient_map(rippled, frame_count/frame_count_div)
-
-
-    return psychedelic
 
 
 # Cartoon functions
--- main.py
+++ main.py
@@ -1,63 +1,28 @@
 import cv2
-import torch
-from options.test_options import TestOptions
-from models import create_model
-from torchvision import transforms
-from util.util import tensor2im
-import sys
 import os
 from datetime import datetime
 import functions
-import mediapipe as mp
-import tensorflow_hub as hub
-import numpy as np
 import tensorflow as tf
-from tensorflow.keras.applications import InceptionV3
 
 
-# Define model parameters
-sys.argv = [
-    'test.py',  # Script name
-    '--name', 'style',  # horse2zebra_pretrained / style_vangogh_pretrained / cartoon / style / psych / dream
-    '--load_size', '512',
-    '--output_height', '512',
-    '--output_width', '512',
-    '--gpu_ids', '',  # 0 for
-    '--no_dropout',
-    '--dream_model_layer', '1',
-    '--face_text', '',  # only for cartoon model
-    '--face_effects', '',  # only for cartoon model
-    '--save_output_path', ''
-]
+#  Optimize tensorflow GPU usage
+tf.config.optimizer.set_jit(True)
+gpus = tf.config.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
 
-opt = TestOptions().parse() # Get default options
-if opt.name == 'cartoon':
-    # Initialize Mediapipe Face Detection
-    mp_face_detection = mp.solutions.face_detection
-    face_detection = mp_face_detection.FaceDetection(min_detection_confidence=0.5)
-elif opt.name == 'style':
-    # Load the pre-trained style transfer model
-    style_transfer_model = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
-    style_image_path = 'input/style.jpg'
-    prev_style_image = cv2.imread(style_image_path)  # Replace with your style image path
-    prev_style_image = cv2.cvtColor(prev_style_image, cv2.COLOR_BGR2RGB)
-elif opt.name == 'dream':
-    # Load pre-trained InceptionV3 model
-    model = InceptionV3(include_top=False, weights='imagenet')
-    dream_layer = model.get_layer(f'mixed{opt.dream_model_layer}')  # Layer to "dream" from
-    dream_model = tf.keras.Model(inputs=model.input, outputs=dream_layer.output)
-elif opt.name != 'psych':
-    # Initialize CycleGAN model
-    model = create_model(opt)  # Create the CycleGAN model
-    model.setup(opt)  # Load the pre-trained weights
+# style_transfer / cyclegan_horse2zebra_pretrained / cyclegan_style_vangogh_pretrained / yolo / psych / dream / cartoon
+model_name = 'yolo_cyclegan_horse2zebra_pretrained'
+img_load_size = 512  # only for style and dream models
+output_width = 1550
+output_height = 850
+save_output_path = ''
+dream_model_layer = '1'  # only for dream model
+face_text = '',  # only for cartoon model
+face_effects = '',  # only for cartoon model
 
-# Define transforms for webcam frames
-transform = transforms.Compose([
-    transforms.ToPILImage(),
-    transforms.Resize((opt.load_size, opt.load_size)),
-    transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-])
+# Define models
+models, params = functions.define_models_params(model_name, img_load_size, output_width, output_height, save_output_path, dream_model_layer)
 
 # initialize the camera (only one camera use port = 0)
 cam_port = 0
@@ -68,13 +33,13 @@ fps = int(cam.get(cv2.CAP_PROP_FPS)) or 30
 
 # Define the codec and create VideoWriter object
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-if opt.save_output_path:
-    print(f'saving output to {opt.save_output_path} folder')
-    os.makedirs(opt.save_output_path, exist_ok=True)
+if save_output_path:
+    print(f'saving output to {save_output_path} folder')
+    os.makedirs(save_output_path, exist_ok=True)
     start_time = datetime.today().strftime('%Y-%m-%d_%H-%M')
-    out = cv2.VideoWriter(f'{opt.save_output_path}/{start_time}.mp4', fourcc, fps, (opt.output_width, opt.output_height))
+    out = cv2.VideoWriter(f'{save_output_path}/{start_time}.mp4', fourcc, fps, (output_width, output_height))
 
-# Runq
+# Run
 frame_count = 0
 while True:
 
@@ -83,84 +48,33 @@ while True:
     if not ret:
         raise ValueError('No camera detected')
 
-    # Resize frame
-    frame = cv2.resize(frame, (opt.load_size, opt.load_size))
+    # Apply models
+    if 'style_transfer' in model_name:
+        frame = functions.transform_frame_style_transfer(models, frame, img_load_size, params['style_image_path'],
+                                                         params['prev_style_image'], output_width, output_height)
 
-    if opt.name == 'psych':
-        output_frame = functions.psychedelic_effect(frame, frame_count, amplitude=20, wavelength=75, frame_count_div=5)
+    if 'cyclegan' in model_name:
+        frame = functions.transform_frame_cyclegan(models, frame, img_load_size, params['opt'], params['transform'],
+                                                   output_width, output_height)
 
-    elif opt.name == 'dream':
-        # Convert frame to RGB
-        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        # Apply DeepDream effect (downscale for efficiency)
-        dreamed_frame = functions.apply_deepdream(rgb_frame, dream_model, img_size=opt.load_size)
-        # Convert back to BGR for OpenCV display
-        output_frame = cv2.cvtColor(dreamed_frame, cv2.COLOR_RGB2BGR)
+    if 'yolo' in model_name:
+        frame = functions.transform_frame_yolo(models, frame)
 
-    elif opt.name == 'cartoon':
-        output_frame = functions.cartoonify(frame)
+    if 'psych' in model_name:
+        frame = functions.transform_frame_pych(frame, frame_count, amplitude=20, wavelength=75, frame_count_div=5)
 
-        # Detect faces
-        if opt.face_effects:
-            results = face_detection.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-            face_coords = []
-            if results.detections:
-                for detection in results.detections:
-                    bbox = detection.location_data.relative_bounding_box
-                    face_coords.append((bbox.xmin, bbox.ymin, bbox.xmin + bbox.width, bbox.ymin + bbox.height))
+    if 'dream' in model_name:
+        frame = functions.transform_frame_dream(models, frame, img_load_size, output_width, output_height)
 
-        # Add math effects
-        if opt.face_text:
-            output_frame = functions.add_math_effect(output_frame, face_coords, text=opt.face_text)
-
-    elif opt.name == 'style':
-
-        # Load new style image
-        style_image = cv2.imread(style_image_path)  # Replace with your style image path
-        # If loading failed, stick to previous style
-        if not isinstance(style_image, np.ndarray):
-            style_image = prev_style_image
-        # If it worked, update previous style
-        elif style_image.shape != prev_style_image.shape:
-            prev_style_image = style_image
-        elif (style_image == prev_style_image).any():
-            prev_style_image = style_image
-
-        style_image = cv2.cvtColor(style_image, cv2.COLOR_BGR2RGB)
-
-        # Convert frame to RGB
-        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-        # Apply style transfer
-        styled_frame = functions.apply_style_transfer(rgb_frame, style_image, style_transfer_model, image_size=opt.load_size)
-
-        # Convert back to BGR for OpenCV display
-        output_frame = cv2.cvtColor(styled_frame, cv2.COLOR_RGB2BGR)
-
-    else:
-        # Convert to rgb
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-        # Transform with model here
-        input_tensor = transform(frame_rgb).unsqueeze(0).to(torch.device("cuda" if opt.gpu_ids else "cpu"))
-
-        # Generate the transformed image
-        model.set_input({'A': input_tensor})  # Set the input image
-        model.test()  # Perform inference
-        output_image = tensor2im(model.get_current_visuals()['fake'])
-
-        # Convert the output image to a format suitable for OpenCV
-        output_frame = cv2.cvtColor(output_image, cv2.COLOR_RGB2BGR)
-
-    # Resize output frame
-    output_frame = cv2.resize(output_frame, (opt.output_width, opt.output_height))
+    # if 'cartoon' in model_name:
+    #     frame = functions.transform_frame_cartoon(frame, face_effects, params['face_detection'], face_text)
 
     # Display the captured frame
-    cv2.imshow('Camera', output_frame)
+    cv2.imshow('Camera', frame)
 
     # Write the frame to the output file
-    if opt.save_output_path:
-        out.write(output_frame)
+    if save_output_path:
+        out.write(frame)
 
     # Press 'q' to exit the loop
     if cv2.waitKey(1) == ord('q'):
@@ -171,6 +85,6 @@ while True:
 
 # Release camera and save file
 cam.release()
-if opt.save_output_path:
+if save_output_path:
     out.release()
 cv2.destroyAllWindows()
